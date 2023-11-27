955252fa8db4298306e83409471e48d776ec89a2
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-11-27 21:58:32 +0100

    summary

--- couplejournal/settings.py
+++ couplejournal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%m5__if=2a!ek#!%gtomhoyu(nqdmrio#go7pxuk2jsq^+x55+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cpj.onrender.com']
 
 
 # Application definition
