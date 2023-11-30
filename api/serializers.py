4b294bdbddf8301318c596b48ecb9c996420c756
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-11-30 07:12:32 +0100

    update Collaborative list get

--- api/serializers.py
+++ api/serializers.py
@@ -46,11 +46,26 @@ class CollaborativeListSerializer(serializers.ModelSerializer):
 class CollaborativeListSerializerExtended(serializers.ModelSerializer):
     listitem_count = serializers.IntegerField()
     done_item_count = serializers.IntegerField()
+    user_name = serializers.SerializerMethodField()
+    has_team = serializers.SerializerMethodField()
+    member1_name = serializers.SerializerMethodField()
+    member2_name = serializers.SerializerMethodField()
 
     class Meta:
         model = CollaborativeList
-        fields = ['id', 'team', 'user', 'title', 'color', 'description', 'listitem_count', 'done_item_count']
+        fields = ['id', 'team', 'user', 'user_name', 'member1_name', 'member2_name', 'title', 'color', 'description', 'listitem_count', 'done_item_count', 'has_team']
 
+    def get_user_name(self, obj):
+        return obj.user.name if obj.user else None
+
+    def get_has_team(self, obj):
+        return obj.team is not None
+
+    def get_member1_name(self, obj):
+        return obj.team.member1.name if obj.team and obj.team.member1 else None
+
+    def get_member2_name(self, obj):
+        return obj.team.member2.name if obj.team and obj.team.member2 else None
 
 
 class CustomAuthTokenSerializer(AuthTokenSerializer):
--- api/views.py
+++ api/views.py
@@ -165,3 +165,4 @@ class UserCollaborativeListsView(generics.ListAPIView):
         )
 
         return queryset
+
