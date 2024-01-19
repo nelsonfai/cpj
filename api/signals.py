54926eb774517ac6cdb316c3f54a7c13e64fb19a
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2024-01-19 10:06:11 +0100

    Update signals.py

--- api/signals.py
+++ api/signals.py
@@ -35,7 +35,7 @@ def habit_completed_notification(sender, instance, created, **kwargs):
             team_member = habit_team.member1 if habit_team.member2 == instance.user else habit_team.member2
 
             if team_member.expo_token:
-                send_message(expo_token=team_member.expo,title='Habit Done',body='Your Partner Just completed the Task')
+                send_message(expo_token=team_member.expo_token,title='Habit Done',body='Your Partner Just completed the Task')
 
 def send_message(expo_token, title, body):
         expo_url = 'https://exp.host/--/api/v2/push/send'
