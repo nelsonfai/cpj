adc85aab6fad0c30e31dc5ec7ae62b69bd5300a2
nelson fai <nelsonfai21@yahoo.com>
2024-03-15 17:12:06 +0000

    modified:   api/signals.py

--- api/signals.py
+++ api/signals.py
@@ -43,10 +43,18 @@ def habit_completed_notification(sender, instance, created, **kwargs):
     if created:
         habit_team = instance.habit.team
         if habit_team: # and instance.user.is_premium:
-            team_member = habit_team.member1 if habit_team.member2 == instance.user else habit_team.member2
-            print(f'-------{team_member.name} just completed the habit: {instance.habit.name}.-------')
+            if habit_team.member2 != instance.user:
+                team_member = habit_team.member1
+                other_member = habit_team.member2
+            else:
+                team_member = habit_team.member2
+                other_member = habit_team.member1
             if team_member.expo_token:
-                send_message(expo_token=team_member.expo_token,title='Habit Done !',body=f'{team_member.name} just completed the habit: {instance.habit.name}.')
+                send_message(
+                    expo_token=team_member.expo_token,
+                    title='Habit Completed!',
+                    body=f'Your partner, {other_member.name}, just completed the habit: {instance.habit.name}.'
+                )
 
 def send_message(expo_token, title, body):
     expo_url = 'https://exp.host/--/api/v2/push/send'
